8e46f1e8a3b0c17c4acbfbad606f07440b43c392
Markus Minkkinen <91621597+HuskyM3@users.noreply.github.com>
2022-12-14 08:47:37 +0200

    new course.js and courseinfo2 ready

--- part2/courseinfo2/src/App.js
+++ part2/courseinfo2/src/App.js
@@ -1,97 +1,59 @@
-const Headder = (props) => {
- const ncourse = props.course
- console.log('check')
-  return (
-    <div>
-      <h1>{ncourse.name}</h1>
-      
-    </div>
-  )
+import Course from './Course'
 
-}
-
-const Content = ({course}) => {
-  const inner = course.parts
-  const result = inner.map(n => 
-    <p key={n.id}> 
-    <Part cont = {n} /> 
-    </p>)
-
-  return (
-    <div>
-    {result }
-    </div>
-  )
-
-}
-const Part = ({cont}) => {
-
-const tent = cont
-  const innerN = tent.name
-  const innerA = tent.exercises
-  return (
-      <>
-        {innerN} {innerA}
-      </>
-  )
-}
-
-const Total = ({course}) => {
-  const totals = course.parts.map(n=> n.exercises)
-  // jostain syystä Total props ei toimi 
-  const sum = totals.reduce((s, p) => s + p,0)
-  
-  return (
-      <p>
-      Number of exercises {sum}
-      </p>
-  )
-}
-
-
-
-const Course = (props) => {
-  const course = props.course
-  return (
-    <div>
-      <Headder course = {course} />
-      <Content course = {course} />
-      <Total course = {course} />
-    </div>
-
-  )
-}
 
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
 
   return (
-    
-      <Course course={course} />
-
-  
-
+    <div>
+      <h1>Web development curriulum </h1>
+      {courses.map(n=> <Course key = {n.id} course={n} />)}
+    </div>
   
   )
 }
---
+++ part2/courseinfo2/src/Course.js
@@ -0,0 +1,57 @@
+const Headder = ({course}) => {
+    //console.log('header')
+    return (
+        <h2>{course}</h2>
+    )
+  
+  }
+  
+  const Content = ({course}) => {
+    const result = course.map(n => 
+      <Part key={n.id} cont = {n} /> )
+      //console.log('content')
+    return (
+      <>
+      {result }
+      </>
+    )
+  
+  }
+  const Part = ({cont}) => {
+  //console.log('part')
+    return (
+        <p>
+          {cont.name} {cont.exercises}
+        </p>
+    )
+  }
+  
+
+
+  const Total = ({course}) => {
+    const sum = course.reduce((s, p) => s + p.exercises,0)
+    //console.log('total')
+    return (
+        <b>
+         total of {sum} exercises
+        </b>
+    )
+  }
+  
+  
+  
+  
+  
+  const Course = ({course}) => {
+    //console.log('toimii')
+    return (
+      <div>
+        <Headder course={course.name} />
+        <Content course = {course.parts} />
+        <Total course = {course.parts} />
+      </div>
+  
+    )
+  }
+  
+  export default Course
