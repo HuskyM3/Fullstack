e6434f163400a8e7cd44731d7c0520aa27033b60
Markus Minkkinen <91621597+HuskyM3@users.noreply.github.com>
2022-12-27 15:28:57 +0200

    2.10 done without case sensitivity

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,9 +1,87 @@
 import { useState } from 'react'
-//import Note from './Note'
+
 
 const Note = ({ note }) => {
   return (
     <div>{note.name} {note.number}</div>
+  )
+}
+
+
+const Filter = ({newS, setNewS}) => {
+  const handleSearchChange = (event) => {
+    console.log(event.target.value)
+    setNewS(event.target.value)
+  }
+  return (
+
+    <div>Filter shown with
+    <input
+    value={newS}
+    onChange={handleSearchChange}
+    />
+</div>
+  )
+
+}
+const PersonForm = ({persons, setNotes, newNote, setNewNote, newNumber, setNewNumber}) => {
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }  
+
+  const addNote = (event) => { // todennäköisesti alkuperäisessä versiossa tässä meni vihkoon 
+    event.preventDefault()
+    const noteObject = {
+      name: newNote, // tämän pitää olla sama mikä on siinä arrayssa johon lisätään
+      number: newNumber,
+      //id: persons.length + 1,
+    }
+    const test = persons.find(n=> n.name === noteObject.name)
+    console.log(test)
+    if (test === undefined ){
+      setNotes(persons.concat(noteObject))
+      setNewNote('')
+      setNewNumber('')
+    }else {
+      alert(noteObject.name + ' is already added to the phonebook')
+    }
+  }
+
+return (
+  <form onSubmit={addNote}>
+  <div>name: 
+    <input
+    value={newNote}
+    onChange={handleNoteChange}
+  /></div>
+  
+  <div>number: 
+  <input 
+  value={newNumber}
+  onChange={handleNumberChange}
+  /></div>
+  <button type="submit">add</button>
+</form>
+
+
+)
+}
+
+const ShowPersons =({persons, newS}) => {
+  const newPersons = persons.filter(name => name.name.includes(newS))
+
+  return (
+    <div>
+    {newPersons.map(note => 
+      <Note key={note.name} note={note} />
+      )}
+      </div>
   )
 }
 
@@ -29,80 +107,27 @@ const App = () => {
   const [newNote, setNewNote] = useState('') 
   const [newNumber, setNewNumber] = useState('') 
   const [newS, setNewS] = useState('') 
-
-
-
-
-  const addNote = (event) => { // todennäköisesti alkuperäisessä versiossa tässä meni vihkoon 
-    event.preventDefault()
-    const noteObject = {
-      name: newNote, // tämän pitää olla sama mikä on siinä arrayssa johon lisätään
-      number: newNumber,
-      //id: persons.length + 1,
-    }
-    const test = persons.find(n=> n.name === noteObject.name)
-    console.log(test)
-    if (test === undefined ){
-      setNotes(persons.concat(noteObject))
-      setNewNote('')
-      setNewNumber('')
-    }else {
-      alert(noteObject.name + ' is already added to the phonebook')
-    }
-  }
-
-  const newPersons = persons.filter(name => name.name.includes(newS))
-
-  // filtteröinti pitää varmaan sitoa Note mappäys juttuun 
-  // saattaa toimia kappaleen esimerkin avulla 
-  // erillinen komponetti showlle ja finderille 
-
-  const handleNumberChange = (event) => {
-    console.log(event.target.value)
-    setNewNumber(event.target.value)
-  }
-  const handleNoteChange = (event) => {
-    console.log(event.target.value)
-    setNewNote(event.target.value)
-  }
-  const handleSearchChange = (event) => {
-    console.log(event.target.value)
-    setNewS(event.target.value)
-  }
   
   return (
     <div>
       <h1>Phonebook</h1>
 
-      <div>search:
-          <input
-          value={newS}
-          onChange={handleSearchChange}
-          />
-      </div>
+      <Filter newS={newS} setNewS={setNewS} />
+
 
       <h2>add a new</h2>
 
-      <form onSubmit={addNote}>
-        <div>name: 
-          <input
-          value={newNote}
-          onChange={handleNoteChange}
-        /></div>
-        
-        <div>number: 
-        <input 
-        value={newNumber}
-        onChange={handleNumberChange}
-        /></div>
-        <button type="submit">add</button>
-      </form>
+      <PersonForm 
+      persons={persons}
+      setNotes={setNotes}
+      newNote={newNote}
+      setNewNote={setNewNote}
+      newNumber={newNumber}
+      setNewNumber={setNewNumber}
+      />
     
-    
-    <h2>Numbers</h2>
-        {newPersons.map(note => 
-          <Note key={note.name} note={note} />
-        )}
+      <h2>Numbers</h2>
+      <ShowPersons persons={persons} newS={newS}/>
     </div>
   )
 }
