13735a3779166d46a589bbddf757cc02e22251b9
Markus Minkkinen <91621597+HuskyM3@users.noreply.github.com>
2022-12-27 14:43:49 +0200

    2.9 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,7 +3,7 @@ import { useState } from 'react'
 
 const Note = ({ note }) => {
   return (
-    <div>{note.name}</div>
+    <div>{note.name} {note.number}</div>
   )
 }
 
@@ -13,55 +13,93 @@ const App = () => {
     {
       //id: 1,
       name: 'Teemu Teekkari',
+      number: 123,
     },
     {
       //id: 2,
       name: 'Anna Arkkari',
+      number: 223,
     },
     {
       //id: 3,
       name: 'new',
+      number: 331,
     }
   ])
   const [newNote, setNewNote] = useState('') 
+  const [newNumber, setNewNumber] = useState('') 
+  const [newS, setNewS] = useState('') 
+
+
+
 
   const addNote = (event) => { // todennäköisesti alkuperäisessä versiossa tässä meni vihkoon 
     event.preventDefault()
     const noteObject = {
       name: newNote, // tämän pitää olla sama mikä on siinä arrayssa johon lisätään
+      number: newNumber,
       //id: persons.length + 1,
     }
 
-    const test = persons.find(n=> n.name === noteObject.name)
-    console.log(test)
-    if (test === undefined ){
+    if (persons.includes(noteObject.name)){
       setNotes(persons.concat(noteObject))
       setNewNote('')
+      setNewNumber('')
     }else {
       alert(noteObject.name + ' is already added to the phonebook')
     }
-
-
   }
 
+  const newPersons = persons.filter(name => name.name.includes(newS))
+
+  // filtteröinti pitää varmaan sitoa Note mappäys juttuun 
+  // saattaa toimia kappaleen esimerkin avulla 
+  // erillinen komponetti showlle ja finderille 
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
   const handleNoteChange = (event) => {
     console.log(event.target.value)
     setNewNote(event.target.value)
+  }
+  const handleSearchChange = (event) => {
+    console.log(event.target.value)
+    setNewS(event.target.value)
   }
   
   return (
     <div>
       <h1>Phonebook</h1>
 
+      <div>search:
+          <input
+          value={newS}
+          onChange={handleSearchChange}
+          />
+      </div>
+
+      <h2>add a new</h2>
+
       <form onSubmit={addNote}>
-        name:<input
+        <div>name: 
+          <input
           value={newNote}
           onChange={handleNoteChange}
-        />
+        /></div>
+        
+        <div>number: 
+        <input 
+        value={newNumber}
+        onChange={handleNumberChange}
+        /></div>
         <button type="submit">add</button>
       </form>
+    
+    
     <h2>Numbers</h2>
-        {persons.map(note => 
+        {newPersons.map(note => 
           <Note key={note.name} note={note} />
         )}
     </div>
@@ -69,56 +107,3 @@ const App = () => {
 }
 
 export default App
-
-
-/*
-
-const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
-
-  const handleNoteChange = (event) => {
-    console.log(event.target.value)
-    setNewName(event.target.value)
-  }
-
-
-  const addNote = (event) => {
-    event.preventDefault()
-    console.log('button clicked', event)
-    setPersons(persons.concat(event))
-    setNewName('')
-  }
-
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      
-      <form onSubmit = {addNote}>
-          name: <input 
-          value={newName}
-          onChange={handleNoteChange}
-          />
-        
-          <button type="submit">add</button>
-          </form>
-
-
-      <h2>Numbers</h2>
-
-
-        {persons.map(name => 
-        <Note key={name.name} note={name.name} />
-        )}
-
-
-    </div>
-  )
-}
-
-export default App
-
-
-*/
