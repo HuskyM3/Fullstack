a4a725f0970f6e7592fd94ec45cc869f80f19a6b
Markus Minkkinen <91621597+HuskyM3@users.noreply.github.com>
2023-01-08 22:19:07 +0200

    2.19 and 2.20 done, filter don't work

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react'
 import axios, { Axios } from 'axios'
 import serverWork from './serverComp'
+import './index.css'
+
 
 
 const Note = ({ note, serverDel }) => {
@@ -13,7 +15,7 @@ const Note = ({ note, serverDel }) => {
 }
 
 
-const Filter = ({newS, setNewS}) => {
+const Filter = ({newS, setNewS}) => { // tämä pitää korjata
   const handleSearchChange = (event) => {
     console.log(event.target.value)
     setNewS(event.target.value)
@@ -29,7 +31,7 @@ const Filter = ({newS, setNewS}) => {
   )
 
 }
-const PersonForm = ({persons, setNotes, newNote, setNewNote, newNumber, setNewNumber}) => {
+const PersonForm = ({persons, setNotes, newNote, setNewNote, newNumber, setNewNumber, setMessage}) => {
 
   const handleNumberChange = (event) => {
     console.log(event.target.value)
@@ -40,29 +42,42 @@ const PersonForm = ({persons, setNotes, newNote, setNewNote, newNumber, setNewNu
     setNewNote(event.target.value)
   }  
 
-  const addNote = (event) => { // todennäköisesti alkuperäisessä versiossa tässä meni vihkoon 
+  const addNote = (event) => { 
     event.preventDefault()
     const noteObject = {
-      name: newNote, // tämän pitää olla sama mikä on siinä arrayssa johon lisätään
+      name: newNote, 
       number: newNumber,
-      //id: persons.length + 1,
+      id: event.toInt 
     }
     const test = persons.find(n=> n.name === noteObject.name)
-    console.log(test)
+    console.log(test) 
     if (test === undefined ){
       serverWork.add(noteObject).then(note =>{
+        console.log(noteObject.name)
         setNotes(persons.concat(noteObject))
         setNewNote('')
         setNewNumber('')
+        PopUpBar(setMessage, noteObject.name, 'added')
       })
+      
 
     }else {
-      alert(noteObject.name + ' is already added to the phonebook')
+      
+      if(window.confirm(`${noteObject.name} update`)){ 
+        serverWork.update(test.id, noteObject).then(m=>
+          setNotes(persons.map(n=> n.id !== test.id ? n : m))
+          ).then(n=>{
+            PopUpBar(setMessage, noteObject.name, 'changed')
+          })
+        setNewNote('')
+        setNewNumber('')
+        }
     }
   }
 
 
 return (
+  
   <form onSubmit={addNote}>
   <div>name: 
     <input
@@ -83,23 +98,67 @@ return (
 }
 
 
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+  if(message.includes('already removed from server')){ //not best solution but working one 
+  return (
+    <div className='error'>
+      {message}
+    </div>
+  )}else{
+    return(
+    <div className='change'>
+    {message}
+  </div>
+    )
+    }
+}
+
+const PopUpBar =(setMessage, noteObject, oType) => {
+  setMessage(`${noteObject} was ${oType}`)
+  setTimeout(()=> {
+    setMessage(null)
+  }, 2000)
+  
+}
 
 
-const ShowPersons =({persons, newS, setNotes, single}) => {
+const ShowPersons =({persons, newS, setNotes, single, setMessage}) => {
   const newPersons = persons.filter(name => name.name.toLowerCase().includes(newS.toLowerCase()))
   const deleteButton = id => {
     const theID = newPersons.find(n=> n.id === id)
     console.log(theID)
     if (window.confirm(`delete ${theID.name} `)) {
-      serverWork.serverDelete(id, theID)
-      setNotes(newPersons.filter(n=> n.id !== id))
+      serverWork.serverDelete(id, theID).then(n=>{
+        PopUpBar(setMessage, theID.name, 'deleted')
+        setNotes(newPersons.filter(n=> n.id !== id))
+      }).catch(err=>{
+        PopUpBar(setMessage, theID.name, 'already removed from server')
+      })
+      
     }
   }
   return (
-    //tähän ehkä pitäis saada päivityetty versio 
     <div>
       <Note note={single} serverDel={() => deleteButton(single.id)} />
       </div>
+  )
+}
+
+
+const Footer = () => {
+  const footerStyle = {
+    color: 'green',
+    fontStyle: 'italic',
+    fontSize: 16
+  }
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>Note app, Department of Test Apps, Wawe university</em>
+    </div>
   )
 }
 
@@ -108,7 +167,8 @@ const App = () => {
   const [persons, setNotes] = useState([])
   const [newNote, setNewNote] = useState('') 
   const [newNumber, setNewNumber] = useState('') 
-  const [newS, setNewS] = useState('') 
+  const [newS, setNewS] = useState('') //filter 
+  const [message, setMessage] = useState(null)
   
 useEffect(() => {
     axios
@@ -121,12 +181,12 @@ useEffect(() => {
   return (
     <div>
       <h1>Phonebook</h1>
-
+      <Notification message={message} />
       <Filter newS={newS} setNewS={setNewS} />
 
 
       <h2>add a new</h2>
-
+      
       <PersonForm 
       persons={persons}
       setNotes={setNotes}
@@ -134,11 +194,16 @@ useEffect(() => {
       setNewNote={setNewNote}
       newNumber={newNumber}
       setNewNumber={setNewNumber}
+      setMessage={setMessage}
       />
     
       <h2>Numbers</h2>
-      {persons.map(n=><ShowPersons key={n.name}  persons={persons} newS={newS} setNotes={setNotes} single={n}/>)}
+      {persons.map(n=><ShowPersons 
+      key={n.name}  persons={persons} 
+      newS={newS} setNotes={setNotes} single={n} 
+      setMessage={setMessage}/>)}
       
+      <Footer />
     </div>
   )
 }
--- part2/phonebook/src/serverComp.js
+++ part2/phonebook/src/serverComp.js
@@ -5,20 +5,19 @@ const baseUrl = 'http://localhost:3001/persons'
 
 const add = noteObject => {
   const request = axios.post(baseUrl, noteObject)
-  return (
-  request.then(response => {
-    //response.data
-    console.log(response)
-  })
-  )
+  return request.then(response => response.data)
+
 }
 const serverDelete = (id, newObject) => {
   const request = axios.delete(`${baseUrl}/${id}`, newObject)
   return request.then(response => response.data)
 }
+const update = (id, newObject) => {
+  const request = axios.put(`${baseUrl}/${id}`, newObject)
+  return request.then(response => response.data)
+}
 
-
-export default {add, serverDelete}
+export default {add, serverDelete, update}
 
 
 
