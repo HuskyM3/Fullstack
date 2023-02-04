d946c6b745a11d5c7881eea16969bace37c78b29
Markus Minkkinen <91621597+HuskyM3@users.noreply.github.com>
2023-02-04 15:23:15 +0200

    add show blogs in users 4.17

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -2,26 +2,30 @@ const blogRouter = require('express').Router()
 //const { response } = require('../app')
 //const blog = require('../models/blog')
 const Blog = require('../models/blog')
-
+const User = require('../models/user')
 
 blogRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})  
+  const blogs = await Blog.find({}).populate('user', {username:1, name:1}) 
   response.json(blogs)
   })
   
-  blogRouter.post('/', async (request, response, next) => {
+  blogRouter.post('/', async (request, response) => {
     
     const body = request.body
+    const user = await User.findById(body.userId)
 
     const note = new Blog({
       title: body.title,
       author: body.author,
       url: body.url,
       likes: body.likes,
+      user: user._id
     })
   
     //try{
       const savedNote = await note.save()
+      user.blogs = user.blogs.concat(savedNote._id)
+      await user.save()
       response.status(201).json(savedNote)
     //} catch(exeption){next(exeption)}
    })
@@ -38,7 +42,7 @@ blogRouter.get('/', async (request, response) => {
     
   
     const updated = await Blog.findByIdAndUpdate(request.params.id, body, { new: true })
-    console.log(updated)
+    //console.log(updated)
     response.status(204).json(updated)
   })
 
