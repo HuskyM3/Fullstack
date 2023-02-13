f7cac53b5ee6b1764e6d6f06b2e0de9e70c0997b
Markus Minkkinen <91621597+HuskyM3@users.noreply.github.com>
2023-02-13 13:15:29 +0200

    minor visual update 6.8 ready

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,53 +1,12 @@
-/*
-import { useSelector, useDispatch } from 'react-redux'
-
-const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
-
-  const vote = (id) => {
-    console.log('vote', id)
-  }
-
-  return (
-    <div>
-      <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
-      <h2>create new</h2>
-      <form>
-        <div><input /></div>
-        <button>create</button>
-      </form>
-    </div>
-  )
-}
-
-export default App
-
-
-*/
-
-
-
-
-
 import Anecdotes from "./components/Anecdotes"
 import NewAnacdote from "./components/NewAnacdote"
 
 const App = () => {
   return(
     <div>
+      <h2>Anecdotes</h2>
       <Anecdotes />
+      <h3>Add new</h3>
       <NewAnacdote/>
     </div>
   )
