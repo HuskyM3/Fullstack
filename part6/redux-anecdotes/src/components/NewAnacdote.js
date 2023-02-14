3ee5275fecfc3496aff091bc72d350be5a847e29
Markus Minkkinen <91621597+HuskyM3@users.noreply.github.com>
2023-02-14 13:34:27 +0200

    add notification 'create new'

--- part6/redux-anecdotes/src/components/NewAnacdote.js
+++ part6/redux-anecdotes/src/components/NewAnacdote.js
@@ -1,5 +1,6 @@
 import { useDispatch } from 'react-redux'
 import { createNote } from '../reducers/anecdoteReducer'
+import { notificationChange } from '../reducers/notificationReducer'
 import services from '../services/anecdotes'
 
 const NewAnecdote = () => {
@@ -11,6 +12,7 @@ const NewAnecdote = () => {
     event.target.note.value = ''
     //const newAnacdote = await services.createNew(content)
     dispatch(createNote(content))
+    dispatch(notificationChange(`You added '${content}'`))
   }
 
   return (
